8460aefa828a33c3afb6c961f24dc29d1bfaa975
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-09 15:22:27 -0400

    Implemented a order_task function that uses a loop. Now, a task cannot start until its prerequisite tasks have been completed. The program repeatedly loops over all of th etasks that are still to be completed, picks an incomplete one, and then calculates when this task can start by computing the starting points and durations of each of its prerequisite tasks.

--- planner.py
+++ planner.py
@@ -1,5 +1,7 @@
 import csv
+from collections import namedtuple
 
+Task = namedtuple("Task", ["title", "duration", "prerequisites"])
 def read_tasks(filename):
     tasks = {}
     for row in csv.reader(open(filename)):
@@ -7,6 +9,25 @@ def read_tasks(filename):
         title = row[1]
         duration = float(row[2])
         prerequisites = set(map(int, row[3].split()))
-        tasks[number] = (title, duration, \
-                        prerequisites)
+        tasks[number] = Task(title, duration, prerequisites)
     return tasks
+
+def order_tasks(tasks):
+    incomplete = set(tasks)
+    completed = set()
+    start_days = {}
+    while incomplete:
+        for task_number in incomplete:
+            task = tasks[task_number]
+            if task.prerequisites.issubset(completed):
+                earliest_start_day = 0
+                for prereq_number in task.prerequisites:
+                    prereq_end_day = start_days[prereq_number] + \
+                                     tasks[prereq_number].duration
+                    if prereq_end_day > earliest_start_day:
+                        earliest_start_day = prereq_end_day
+                start_days[task_number] = earliest_start_day
+                incomplete.remove(task_number)
+                completed.add(task_number)
+                break
+    return start_days
