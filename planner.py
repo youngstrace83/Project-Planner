fc5e749ea0b3086ae963b5a84b631f7b813380c5
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-09 14:11:02 -0400

    Set prerequisites row to 'int'

--- planner.py
+++ planner.py
@@ -6,7 +6,7 @@ def read_tasks(filename):
         number = int(row[0])
         title = row[1]
         duration = float(row[2])
-        prerequisites = row[3]
+        prerequisites = set(map(int, row[3].split()))
         tasks[number] = (title, duration, \
                         prerequisites)
     return tasks
