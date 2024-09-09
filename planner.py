10cec7b3df9e87395c40528527ad96a2e84ba8ca
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-09 13:27:36 -0400

    Converted number to "int" and duration to "float"

--- planner.py
+++ planner.py
@@ -1,3 +1,12 @@
 import csv
 
-
+def read_tasks(filename):
+    tasks = {}
+    for row in csv.reader(open(filename)):
+        number = int(row[0])
+        title = row[1]
+        duration = float(row[2])
+        prerequisites = row[3]
+        tasks[number] = (title, duration, \
+                        prerequisites)
+    return tasks
